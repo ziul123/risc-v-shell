ce05e797266dc3d8e33cd12df1797e41035b1369
Luiz <luizcsnvartuli@gmail.com>
2024-11-13 18:33:25 -0300

    agora em formato de ecall

--- str-edit.s
+++ str-edit.s
@@ -16,7 +16,7 @@ overflow_msg: .string "buffer overflow: line at max characters\n"
 lf_det: .string "linefeed detected!\n"
 
 
-.include "MACROSv21.s"
+.include "MACROSv24.s"
 
 
 .macro print_str_at_prompt(%str,%prompt_pos,%color)
@@ -33,24 +33,45 @@ lf_det: .string "linefeed detected!\n"
 
 .text
 
-li a0, TERM_COLOR
-jal print_prompt
-li a0, TERM_COLOR
-jal print_cursor
+#li a0, TERM_COLOR
+#jal print_prompt
+
+# Read String Interativo
+# a0 = end inicio
+# a1 = tam max string
+# a2 = x
+# a3 = y
+# a4 = cores
+# a5 = num de caracteres digitados
+
+la a0, buffer
+li a1, 40
+li a2, 0
+li a3, 0
+li a4, TERM_COLOR
+call iReadString
+
+iReadString: addi sp, sp, -24 # aloca espaco
+sw ra, 0(sp)	# salva ra
+sw s0, 4(sp)	# salva s0
+sw s1, 8(sp)	# salva s1
+sw s2, 12(sp)	# salva s2
+sw s3, 16(sp)	# salva s3
+sw s4, 20(sp)	# salva s4
+mv s0, a0			# endereco inicial
+mv s1, a1			# tamanho maximo
+mv s2, a2			# x 
+mv s3, a3			# y
+mv s4, a4			# cor
+li a5, 0			# zera contador de caracteres digitados
+
+mv a0, s4
+jal printCursor
 
 
-l0:
-jal char_treatment
-print_str_at_prompt(buffer, prompt_pos, TERM_COLOR)
-li a0, TERM_COLOR
-jal print_cursor
-
-j l0
 
 # le char e faz o tratamento necessario
-char_treatment:
-addi sp, sp, -4
-sw ra, 0(sp)
+loopTreatChar:
 
 jal readChar
 
@@ -62,41 +83,38 @@ beq a0, t0, char_lf
 
 # char printavel
 # adiciona char no buffer e incrementa posicao
-la t0, buffer
-la t1, buf_pos
-lbu t2, 0(t1)
-li t3, BUF_MAX
-beq t2, t3, char_treat.buf_over
-add t0, t0, t2
-sb a0, 0(t0)
-addi t2, t2, 1
-sb t2, 0(t1)
+beq a5, s1, loopTreatChar	# caso tenha o maximo de caracteres, ignora
+add t0, s0, a5	# posicao no buffer
+sb a0, 0(t0)		# escreve caractere no buffer
 
-la t0, cursor_pos
-lhu t1, 0(t0)
-addi t1, t1, 8
-sh t1, 0(t0)
-j char_treat.end
+# desenha char
+slli t1, a5, 3	# calcula offset do x do caractere
+add a1, t1, s2	# calcula x do caractere
+mv a2, s3				# mesmo y
+mv a3, s4				# mesma cor
+li a4, 0				# frame 0
+jal printChar		# imprime char
+
+addi a5, a5, 1	# incrementa contador de caracteres
+
+mv a0, s4
+jal printCursor
+
+j loopTreatChar
 
 char_bs:
-la t0, buffer
-la t1, buf_pos
-lbu t2, 0(t1)
-beqz t2, char_treat.end
-add t0, t0, t2
-sb zero, 0(t0)
-addi t2, t2, -1
-sb t2, 0(t1)
+beqz a5, loopTreatChar # se nao tem caractere, pula
+add t0, s0, a5 # posicao no buffer
+sb zero, 0(t0) # escreve \0 no buffer
 
 # apaga o cursor
 mv a0, zero
-jal print_cursor
+jal printCursor
+addi a5, a5, -1
+mv a0, s4
+jal printCursor
 
-la t0, cursor_pos
-lhu t1, 0(t0)
-addi t1, t1, -8
-sh t1, 0(t0)
-j char_treat.end
+j loopTreatChar
 
 char_lf:
 li a7, 31
@@ -105,15 +123,19 @@ li a1, 300
 li a2, 1
 li a3, 200
 ecall
-j char_treat.end
+j fimiReadString
 
-char_treat.buf_over:
-li a7, 4
-la a0, overflow_msg
-ecall
-char_treat.end:
+
+fimiReadString: add t0, s0, a5
+sb zero, 0(t0)
+
 lw ra, 0(sp)
-addi sp, sp, 4
+lw s0, 4(sp)
+lw s1, 8(sp)
+lw s2, 12(sp)
+lw s3, 16(sp)
+lw s4, 20(sp)
+addi sp, sp, -24
 ret
 
 # escreve o prompt na tela
@@ -134,18 +156,42 @@ ret
 
 # escreve o cursor na tela
 # a0 = cor
-print_cursor:
-addi sp, sp, -4
-sw ra, 0(sp)
-mv a3, a0
-li a0, CURSOR
-la t0, cursor_pos
-lhu a1, 0(t0)
-lhu a2, 2(t0)
-li a4, 0
-jal printChar
-lw ra, 0(sp)
+printCursor:
+beq a5, s1, fimprintCursor	# ignora caso ja tenha escrito maximo de caracteres
+addi sp, sp, -4 # aloca espaco
+sw ra, 0(sp)		# salva ra
+mv a3, a0				# cor
+li a0, CURSOR		# caractere a ser impresso
+slli a1, a5, 3	# calcula offset do x do cursor
+add a1, a1, s2	# calcula x do cursor
+mv a2, s3				# mesmo y
+li a4, 0				# frame 0
+jal printChar		# imprime cursor
+lw ra, 0(sp)		# recupera ra
 addi sp, sp, 4
+fimprintCursor:
 ret
 
-.include "SYSTEMv21.s"
+#addi sp, sp, -4
+#sw ra, 0(sp)
+#mv a3, a0
+#li a0, CURSOR
+#la t0, cursor_pos
+#lhu a1, 0(t0)
+#lhu a2, 2(t0)
+#li a4, 0
+#jal printChar
+#lw ra, 0(sp)
+#addi sp, sp, 4
+#ret
+
+##################################
+# Read String Interativo
+# a0 = end inicio
+# a1 = tam max string
+# a2 = x
+# a3 = y
+# a4 = cores
+##################################
+
+.include "SYSTEMv24.s"
